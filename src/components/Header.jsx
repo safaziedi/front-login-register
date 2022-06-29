bdeee4c89920206aab764059f6ca7d152b2f27fd
safaziedi <safaziedi070@gmail.com>
2022-06-29 16:13:50 +0100

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      <div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+          <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <a href="#" class="nav-link px-2 link-secondary">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link px-2 link-dark">
+                To Do List
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link px-2 link-dark">
+                About
+              </a>
+            </li>
+          </ul>
+
+          <div class="col-md-3 text-end">
+            <Link to="/login">
+              {" "}
+              <button type="button" class="btn btn-outline-primary me-2">
+                Login
+              </button>
+            </Link>
+
+            <Link to="/register">
+              <button type="button" class="btn btn-primary">
+                Sign-up
+              </button>
+            </Link>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
