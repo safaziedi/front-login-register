6604fbd6d8e663d89c78b356c92c43af54c7ddd5
safaziedi <safaziedi070@gmail.com>
2022-06-29 18:58:05 +0100

    add sign-up component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
           <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
             <li>
-              <a href="#" class="nav-link px-2 link-secondary">
+              <a href="/" class="nav-link px-2 link-secondary">
                 Home
               </a>
             </li>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+
+
+
+
+
+
+
+import React from "react";
+import "./Login.css";
+import profile from "./../images/user.png";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <div class="form-signin w-100 m-auto">
+      <form>
+        <img class="mb-4" src={profile} alt="" width="100" height="100" />
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div class="form-floating">
+          <input
+            type="email"
+            class="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div><br />
+        <button class="w-100 btn btn-lg btn-primary" type="submit">
+          Sign Up 
+        </button>
+        <p class="mt-5 mb-3 text-muted">&copy; Ziadi Safa</p>
+      </form>
+    </div>
+  );
 }
 
 export default Register
+
