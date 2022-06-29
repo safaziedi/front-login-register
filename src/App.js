2cd57b6f482201a9f4bb621e1171c6c95ea3b16f
safaziedi <safaziedi070@gmail.com>
2022-06-29 15:49:54 +0100

    add sign-in form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
-
+import Login from './components/Login';
+import Register from './components/Register';
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+     <Header /> 
+     <div className='main'>
+        <Routes>
+          <Route   path="/"  element={ <Login /> }   />
+          <Route   path="/login" element={ <Login /> }   />
+          <Route   path="/register" element={ <Register /> }   />
+        </Routes> 
+      </div>
     </div>
   );
 }
